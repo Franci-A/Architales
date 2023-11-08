0cea043e1b3ffa8094d85ead72d807b4c86e8700
Luca Schifano <luca.schifano005@gmail.com>
2023-11-08 16:51:01 +0100

    Preview cube and grid

--- Assets/Objects/Piece/Editor/PieceEditorWindow.cs
+++ Assets/Objects/Piece/Editor/PieceEditorWindow.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.PackageManager.UI;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class PieceEditorWindow : EditorWindow
 {
+    //Checkboxes grid
     int gridSize = 5;
     float buttonSpace = 15;
-
     bool[,] checkBoxes = new bool[5,5];
 
+    //Preview
+    GameObject gameObject;
+    GameObject[] arrayGO;
+    Editor gameObjectEditor;
 
     [MenuItem("Tools/Piece Editor")]
     public static void ShowWindow()
@@ -21,6 +25,8 @@ public class PieceEditorWindow : EditorWindow
 
     void OnGUI()
     {
+        #region Checkboxes
+
         GUILayout.Label("Base Settings", EditorStyles.boldLabel);
 
         EditorGUILayout.Space();
@@ -34,11 +40,57 @@ public class PieceEditorWindow : EditorWindow
             EditorGUILayout.BeginHorizontal();
             for (int y = 0; y < gridSize; y++)
             {
-                checkBoxes[x, y] = EditorGUILayout.Toggle(checkBoxes[x,y], GUILayout.Width(buttonSpace), GUILayout.Height(buttonSpace));
+                if(x == 2 && y == 2) checkBoxes[x, y] = EditorGUILayout.Toggle(true, GUILayout.Width(buttonSpace), GUILayout.Height(buttonSpace));
+                else checkBoxes[x, y] = EditorGUILayout.Toggle(checkBoxes[x,y], GUILayout.Width(buttonSpace), GUILayout.Height(buttonSpace));
                 i++;
             }
             EditorGUILayout.EndHorizontal();
             EditorGUILayout.EndVertical();
+        }
+        #endregion
+
+        EditorGUILayout.Space();
+
+        EditorGUI.BeginChangeCheck();
+
+        Rect rect = new Rect(0,0,10,10);
+
+        gameObject = (GameObject)EditorGUILayout.ObjectField(gameObject, typeof(GameObject), true);
+
+        arrayGO = new GameObject[gridSize * gridSize];
+        int id = 0;
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                if (checkBoxes[i, j])
+                {
+                    GameObject go = gameObject;
+                    arrayGO[id] = go;
+                }
+                id++;
+            }
+        }
+
+        
+        EditorGUILayout.Space();
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            if (gameObjectEditor != null) DestroyImmediate(gameObjectEditor);
+        }
+
+        GUIStyle bgColor = new GUIStyle();
+        bgColor.normal.background = EditorGUIUtility.whiteTexture;
+
+        if (gameObject != null)
+        {
+            if (gameObjectEditor == null)
+                gameObjectEditor = Editor.CreateEditor(arrayGO);
+
+
+            
+            gameObjectEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(250, 250), bgColor);
         }
 
     }
