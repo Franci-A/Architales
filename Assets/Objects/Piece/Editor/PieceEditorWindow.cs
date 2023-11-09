b4663089123cefe95af14fbc20736a4dd9d0bcf2
Luca Schifano <luca.schifano005@gmail.com>
2023-11-09 15:11:49 +0100

    3d Block + reset

--- Assets/Objects/Piece/Editor/PieceEditorWindow.cs
+++ Assets/Objects/Piece/Editor/PieceEditorWindow.cs
@@ -1,5 +1,7 @@
+using log4net.Util;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.CodeEditor;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -9,16 +11,15 @@ public class PieceEditorWindow : EditorWindow
     //Checkboxes grid
     int gridSize = 5;
     float buttonSpace = 15;
-    bool[,] checkBoxes = new bool[5,5];
-    bool[,] saveCheckBoxes = new bool[5,5];
+    bool[,,] checkBoxes = new bool[5,5,5];
+    bool[,,] saveCheckBoxes = new bool[5,5,5];
 
     //Preview
-    //CubeEditor cubeEditor;
-    GameObject cubeEditor;
-    GameObject[,] arrayGO = new GameObject[5, 5];
-    Editor gameObjectEditor;
+    GameObject cube;
+    GameObject[,,] arrayGO = new GameObject[5, 5, 5];
 
-    Mesh newMesh;
+    //Height
+    int height = 0;
 
     [MenuItem("Tools/Piece Editor")]
     public static void ShowWindow()
@@ -29,89 +30,123 @@ public class PieceEditorWindow : EditorWindow
 
     void OnGUI()
     {
-        #region Checkboxes
-
-        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-
         EditorGUILayout.Space();
-        EditorGUILayout.LabelField("Piece Builder");
+        GUILayout.Label("Cube", EditorStyles.boldLabel);
         EditorGUILayout.Space();
 
+        cube = (GameObject)EditorGUILayout.ObjectField(cube, typeof(GameObject), true);
+        EditorGUILayout.Space(20);
 
-        for (int i = 0, x = 0; x < gridSize; x++)
+
+        if (cube == null) EditorGUILayout.LabelField("Please set cube 1x1x1");
+        else
         {
-            EditorGUILayout.BeginVertical();
-            EditorGUILayout.BeginHorizontal();
-            for (int y = 0; y < gridSize; y++)
+
+            #region Checkboxes
+
+            GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Piece Builder");
+            EditorGUILayout.Space();
+
+
+            for (int i = 0, x = 0; x < gridSize; x++)
             {
-                checkBoxes[x, y] = EditorGUILayout.Toggle(checkBoxes[x,y], GUILayout.Width(buttonSpace), GUILayout.Height(buttonSpace));
-                i++;
+                EditorGUILayout.BeginVertical();
+                EditorGUILayout.BeginHorizontal();
+                for (int y = 0; y < gridSize; y++)
+                {
+                    checkBoxes[height, x, y] = EditorGUILayout.Toggle(checkBoxes[height, x, y], GUILayout.Width(buttonSpace), GUILayout.Height(buttonSpace));
+                    i++;
+                }
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.EndVertical();
             }
-            EditorGUILayout.EndHorizontal();
-            EditorGUILayout.EndVertical();
-        }
-        #endregion
+            #endregion
 
-        EditorGUILayout.Space();
+            EditorGUILayout.Space();
 
-        EditorGUI.BeginChangeCheck();
+            EditorGUI.BeginChangeCheck();
 
-        Rect rect = new Rect(0,0,10,10);
-
-        cubeEditor = (GameObject)EditorGUILayout.ObjectField(cubeEditor, typeof(GameObject), true);
-        EditorUtility.SetDirty(cubeEditor);
-        newMesh = (Mesh)EditorGUILayout.ObjectField(newMesh, typeof(Mesh), true);
-
-        if (cubeEditor != null)
-        {
-            string path = "Assets/Objects/Piece/Editor/CubeEditor.prefab";
-            var go = PrefabUtility.LoadPrefabContents(path);
-
-
-
-            PrefabUtility.SaveAsPrefabAsset(go, path);
-
-
+            #region Spawn Blocks
             for (int i = 0; i < gridSize; i++)
             {
                 for (int j = 0; j < gridSize; j++)
                 {
-                    if(checkBoxes[i, j] != saveCheckBoxes[i, j])
+                    if (checkBoxes[height, i, j] != saveCheckBoxes[height, i, j])
                     {
-                        saveCheckBoxes[i, j] = checkBoxes[i, j];
-                        if (checkBoxes[i, j]) arrayGO[i, j] = go.GetComponent<CubeEditor>().UpdateCubeList(new Vector2(i, j), checkBoxes[i, j]);
-                        else 
+                        saveCheckBoxes[height, i, j] = checkBoxes[height, i, j];
+                        if (checkBoxes[height, i, j]) arrayGO[height, i, j] = UpdateCubeList(new Vector2(i, j), checkBoxes[height, i, j]);
+                        else
                         {
-                            DestroyImmediate(arrayGO[i, j]); 
-                            arrayGO[i, j] = null;
+                            DestroyImmediate(arrayGO[height, i, j]);
+                            arrayGO[height, i, j] = null;
                         }
                     }
                 }
             }
-
-        }
-
-        EditorGUILayout.Space();
-
-        if (EditorGUI.EndChangeCheck())
-        {
-            if (gameObjectEditor != null) DestroyImmediate(gameObjectEditor);
-        }
-
-        GUIStyle bgColor = new GUIStyle();
-        bgColor.normal.background = EditorGUIUtility.whiteTexture;
-
-        if (cubeEditor != null)
-        {
-            if (gameObjectEditor == null)
-                gameObjectEditor = Editor.CreateEditor(cubeEditor);
+            #endregion
 
 
-            
-            gameObjectEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(250, 250), bgColor);
+            #region Height
+
+            EditorGUILayout.Space(20);
+            GUILayout.Label("Heigth", EditorStyles.boldLabel);
+
+            EditorGUILayout.Space();
+            var style = new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter };
+            if (GUILayout.Button("+")) height++;
+            EditorGUILayout.LabelField(height.ToString(), style);
+            if (GUILayout.Button("-")) height--;
+
+            height = Mathf.Clamp(height, 0, 4);
+            #endregion
+
+
+            EditorGUILayout.Space(20);
+            GUILayout.Label("Reset", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Reset")) ResetBlock();
         }
 
     }
 
+    public GameObject UpdateCubeList(Vector3 pos, bool isActive)
+    {
+        if (isActive)
+        {
+
+                var go = Instantiate(cube, new Vector3(pos.x, height, pos.y), new Quaternion());
+
+                return go;
+        }
+        else
+        {
+
+                DestroyImmediate(arrayGO[height, (int)pos.x, (int)pos.y]);
+
+            
+
+        }
+        return null;
+    }
+
+    private void ResetBlock()
+    {
+        height = 0;
+
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                for (int k = 0; k < gridSize; k++)
+                {
+                    checkBoxes[i, j, k] = false;
+                    DestroyImmediate(arrayGO[i, j, k]);
+                    arrayGO[i, j, k] = null;
+                }
+            }
+        }
+    }
 
 }
