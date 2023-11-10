bef4ac8207f8c1717846f48e777c2481372ac739
Luca Schifano <luca.schifano005@gmail.com>
2023-11-10 15:19:59 +0100

    GameOver + GameManager

---
+++ Assets/Objects/Game/GameOverScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+
+    public void Restart() 
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void BackToMenu()
+    {
+        Debug.Log("BackToMenu :D");
+        //SceneManager.LoadScene(menu);
+    }
+}
