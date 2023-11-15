a5021892a76fff53b89b7bc099794eb747a75270
Luca Schifano <luca.schifano005@gmail.com>
2023-11-15 09:31:13 +0100

    Change piece hierarchy + animation

--- Assets/Objects/Piece/Piece.cs
+++ Assets/Objects/Piece/Piece.cs
@@ -8,6 +8,7 @@ public class Piece : MonoBehaviour
     [SerializeField] private GridData gridData;
     [SerializeField] private ResidentHandler cubePrefab;
     [SerializeField] private PieceHappinessHandler happinessHandler;
+    [SerializeField] private Transform visualTrans;
 
     List<Cube> cubes = new List<Cube>();
     Resident currentResident;
@@ -17,7 +18,7 @@ public class Piece : MonoBehaviour
     {
         foreach (var cube in cubes)
         {
-            var cubeGO = Instantiate<ResidentHandler>(cubePrefab, transform.position + cube.pieceLocalPosition, transform.rotation, transform);
+            var cubeGO = Instantiate<ResidentHandler>(cubePrefab, visualTrans.position + cube.pieceLocalPosition, visualTrans.rotation, visualTrans);
             cubeGO.SetResident(currentResident);
             cubeGO.parentPiece = this;
             cube.gridPosition = gridData.WorldToGridPosition(transform.position) + cube.pieceLocalPosition;
@@ -44,9 +45,9 @@ public class Piece : MonoBehaviour
 
         currentResident = piece.resident;
 
-        for (int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < visualTrans.childCount; i++)
         {
-            Destroy(transform.GetChild(i).gameObject);
+            Destroy(visualTrans.GetChild(i).gameObject);
         }
 
         cubes = piece.cubes;
@@ -56,9 +57,9 @@ public class Piece : MonoBehaviour
     {
         if(_cubes.Count < 0) return;
 
-        for (int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < visualTrans.childCount; i++)
         {
-            Destroy(transform.GetChild(i).gameObject);
+            Destroy(visualTrans.GetChild(i).gameObject);
         }
 
         cubes = _cubes;
