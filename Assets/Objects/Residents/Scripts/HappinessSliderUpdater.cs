793ec99c6ee7c27348a5eea983595a2a52ee133e
Franci-Algera <algera.franci@gmail.com>
2023-11-15 09:48:52 +0100

    Fix : slider default value

--- Assets/Objects/Residents/Scripts/HappinessSliderUpdater.cs
+++ Assets/Objects/Residents/Scripts/HappinessSliderUpdater.cs
@@ -8,17 +8,28 @@ public class HappinessSliderUpdater : MonoBehaviour
     [SerializeField] private Slider happySlider;
 
     [SerializeField] private IntVariable numberHappyResidents;
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite happyImage;
+    [SerializeField] private Sprite angryImage;
 
 
 
     private void Start()
     {
         numberHappyResidents.OnValueChanged.AddListener(UpdateSliderValue);
+        UpdateSliderValue();
     }
 
     public void UpdateSliderValue()
     {
         happySlider.value = numberHappyResidents.value;
+        if(happySlider.value >= 0)
+        {
+            image.sprite = happyImage;
+        }else
+        {
+            image.sprite = angryImage;
+        }
     }
 
     private void OnDestroy()
