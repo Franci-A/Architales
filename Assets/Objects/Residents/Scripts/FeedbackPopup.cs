83db291ad1a4728799c25a19de14ab885d82ed6b
Franci-Algera <algera.franci@gmail.com>
2023-11-16 09:25:58 +0100

    Feedback popup small improvement

--- Assets/Objects/Residents/Scripts/FeedbackPopup.cs
+++ Assets/Objects/Residents/Scripts/FeedbackPopup.cs
@@ -1,24 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 using UnityEngine.UI;
 
 public class FeedbackPopup : MonoBehaviour
 {
-    [SerializeField] private Renderer plane;
-    [SerializeField] private Material happy;
-    [SerializeField] private Material angry;
-
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite happy;
+    [SerializeField] private Sprite angry;
+    [SerializeField] private float animTime = 1f;
 
     public void InitPopup(bool isPositive)
     {
-        if(isPositive) 
+
+        image.material.color = Color.white;
+        if (isPositive) 
         { 
-            plane.material = happy; 
+            image.sprite = happy; 
         }
         else
         {
-            plane.material = angry;
+            image.sprite = angry;
         }
+        image.color = Color.clear;
+        canvas.transform.DOLocalMoveY(1, animTime);
+        image.DOColor(Color.white, animTime);
+    }
+
+    public void DestroyPopup()
+    {
+        if (DOTween.IsTweening(canvas.transform))
+            canvas.transform.DOKill();
+
+        canvas.transform.DOLocalMoveY(0, animTime/2);
+        image.DOColor(Color.clear, animTime/2);
+
+        Destroy(gameObject, animTime/2);
+    }
+
+    private void OnDestroy()
+    {
+        if (DOTween.IsTweening(canvas.transform))
+            canvas.transform.DOKill();
     }
 }
