fc41aa27987df683fce288706114418998e8b432
Luca Schifano <luca.schifano005@gmail.com>
2023-12-14 09:28:36 +0100

    Add Music + transition

---
+++ Assets/Audio/AudioScript/MusicTention.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTention : MonoBehaviour
+{
+    private int currentHigh;
+    private HighState state;
+
+    [Header("Low Tention")]
+    [SerializeField] AudioClip LowTensionMusic;
+    [SerializeField] AudioClip TransitionToMedMusic;
+    [SerializeField] int levelToMedium;
+
+    [Header("Medium Tention")]
+    [SerializeField] AudioClip MedTensionMusic;
+    [SerializeField] AudioClip TransitionToHighMusic;
+    [SerializeField] int levelToHigh;
+
+    [Header("High Tention")]
+    [SerializeField] AudioClip HighTensionMusic;
+    
+    enum HighState
+    {
+        Low,
+        Medium,
+        High
+    }
+
+
+    private void Start()
+    {
+        Grid3DManager.Instance.OnLayerCubeChange += UpdateMaximumHigh;
+        AudioManager.Instance.PlayMusic(LowTensionMusic);
+    }
+
+    void UpdateMaximumHigh(int higherValue)
+    {
+        currentHigh = higherValue;
+        CheckMusicChange();
+    }
+
+    void CheckMusicChange()
+    {
+        if (currentHigh > levelToHigh && state == HighState.Medium)
+        {
+            StartCoroutine(SwitchSound(TransitionToHighMusic, HighTensionMusic));
+            state = HighState.High;
+        }
+        else if (currentHigh > levelToMedium && state == HighState.Low)
+        {
+            StartCoroutine(SwitchSound(TransitionToMedMusic, MedTensionMusic));
+            state = HighState.Medium;
+        }
+
+    }
+
+    IEnumerator SwitchSound(AudioClip transition, AudioClip music)
+    {
+        AudioManager.Instance.PlayMusic(transition);
+
+        yield return new WaitForSeconds(transition.length);
+
+        AudioManager.Instance.PlayMusic(music);
+
+    }
+}
