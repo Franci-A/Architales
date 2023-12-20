1f3830e17772824f99b13ba0f85526a3a8e0ebe1
illianes <pierremariotte98@gmail.com>
2023-12-20 11:48:05 +0100

    EventAnimation

--- Assets/Objects/Event/EventAnimation.cs
+++ Assets/Objects/Event/EventAnimation.cs
@@ -1,43 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class EventAnimation : MonoBehaviour
 {
+    [Header("Position")]
     private CanvasGroup canvasGroup;
-
-    private float targetLerp;
-    private float targetLerpScale;
+    private float targetLerp, targetLerpScale;
+    private Vector3 startPosition;
 
     [SerializeField] private GameObject crystalBall;
     [SerializeField] private GameObject eventText;
 
+    private bool initiate, startSlide, endSlide = false;    
 
-    private bool startSlide = false;
+    [SerializeField] private GameObject toDesactive;
+
+    [Header("Light")]
+    private float exposureStart = 1.681691f;
+    [SerializeField] private float exposureEnd = 1.25f;
+
+    [SerializeField] private Light ambianceLight;
+    private Color startColor;
+    [SerializeField] private Color endColor;
+
+    private void OnEnable()
+    {
+        initiate = true;
+    }
+
 
     private void Start()
     {
+        startColor = ambianceLight.color;
         canvasGroup = GetComponent<CanvasGroup>();
+        startPosition = eventText.transform.position;
     }
 
     private void Update()
     {
-        StartUI();
+        if (initiate) 
+            StartUI();
 
         if (startSlide)
             Slide();
 
-        
+        if (endSlide)
+            EndUI();
     }
 
-    private void StartUI()
+    public void StartUI()
     {
         if (canvasGroup.alpha < 1f)
-            canvasGroup.alpha += 0.01f;
+        {
+            canvasGroup.alpha += Time.deltaTime;
+            RenderSettings.skybox.SetFloat("_Exposure", Mathf.Lerp (exposureStart, exposureStart - exposureEnd, canvasGroup.alpha));
+            ambianceLight.color = Color.Lerp(startColor, endColor, canvasGroup.alpha);
+        }
         else
         {
-            //start = false;
+            initiate = false;
             StartCoroutine(Animation());
         }
     }
@@ -50,17 +74,18 @@ public class EventAnimation : MonoBehaviour
 
     private void Slide()
     {
-        
-
-        if (targetLerp < 1)
+        if (eventText.transform.localScale.x > 0.01)
         {
             targetLerp += Time.deltaTime * 0.1f;
             targetLerpScale += Time.deltaTime;
         }
         else
         {
+            canvasGroup.alpha = 0f;
+            targetLerp = 0f;
+            targetLerpScale = 0f;
+
             startSlide = false;
-            targetLerp = 0;
         }
 
         var minPos = eventText.transform.position;
@@ -69,7 +94,31 @@ public class EventAnimation : MonoBehaviour
         var targetPosition = Vector3.Lerp(minPos, maxPos, targetLerp);
         eventText.transform.position = targetPosition;
 
-        var targetScale = Mathf.Lerp(0, 1, 1 - targetLerpScale);
+        var targetScale = Mathf.Lerp(1, 0, targetLerpScale);
         eventText.transform.localScale = new Vector3 (targetScale, targetScale, 0);
+    }
+
+    public void EndEvent()
+    {
+        endSlide = true;
+    }
+
+    private void EndUI()
+    {
+        
+        if (targetLerp < 1) 
+        {
+            targetLerp += Time.deltaTime;
+            RenderSettings.skybox.SetFloat("_Exposure", Mathf.Lerp(exposureStart - exposureEnd, exposureStart, targetLerp));
+            ambianceLight.color = Color.Lerp(endColor, startColor, targetLerp);
+        }
+        else
+        {
+            endSlide = false;
+            targetLerp = 0;
+            eventText.transform.localScale = new Vector3(1, 1, 1);
+            eventText.transform.position = startPosition;
+            toDesactive.SetActive(false);
+        }
     }
 }
